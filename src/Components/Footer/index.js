cca22f5baf47c36e0429e6350ada761ab8b53695
ismaeljunior <ifsjunior2@gmail.com>
2023-08-23 21:46:31 -0300

    feat(Component Foot) Ajuste nos links das redes.

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import logo from "../../Assets/logofooter.png";
-import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return(
@@ -10,12 +11,20 @@ const Footer = () => {
             <Container>
                 <Row className="text-center py-1">
                     <Col xs={12} lg={6} className="text-lg-start  mb-lg-0">
-                        <Image src={logo}/>
+                        {/* <Image src={logo}/> */}
+                        <Link to={'/'}>
+                        <img
+                        src={logo}
+                        width="100"
+                        className="Navbarlogo"
+                        alt="PR"
+                        /> 
+                        </Link>                       
                     </Col>
                     <Col xs={12} lg={6} className="fs-1 pt-3 d-flex align-items-center justify-content-center justify-content-lg-end">
-                        <FaFacebook className="me-3" color="#B8860B"/>
-                        <FaInstagram className="me-3" color="#B8860B"/>
-                        <FaYoutube color="#B8860B"/>
+                        <a href="https://www.instagram.com/pessoaeribeiro/"><FaFacebook className="me-3" color="#B8860B" /> </a>
+                        <a href="https://www.instagram.com/pessoaeribeiro/"><FaInstagram className="me-3" color="#B8860B"  /></a>
+                        {/* <FaYoutube color="#B8860B"/> */}
                     </Col>               
                 </Row>
             </Container>
